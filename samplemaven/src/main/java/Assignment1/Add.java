52a107560f595e7b4c8688d3d22a62daace08178
USER <USER@192.168.0.109>
2024-12-03 15:25:16 +0300

    add

---
+++ samplemaven/src/main/java/Assignment1/Add.java
@@ -0,0 +1,13 @@
+package Assignment1;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int a=5;
+  int b=5;
+  int c=a+b;
+  System.out.println(c);
+	}
+
+}
